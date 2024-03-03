0864b98b07f28dc851cfb858bc6b08f95175bb7e
ChamillatulLuqianaAgustin <chamillaluqiana@gmail.com>
2024-03-03 23:24:13 +0700

    Perubahan pada praktikum 6 (Eloquent ORM)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +25,6 @@ Route::get('/level', [LevelController::class, 'index']);
 
 //Praktikum 5
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+//Praktikum 6
+Route::get('/user', [UserController::class, 'index']);
