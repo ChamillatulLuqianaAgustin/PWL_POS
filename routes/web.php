900a5d81fe8b8c66d2dbaedb0c075cd46baab7bb
ChamillatulLuqianaAgustin <chamillaluqiana@gmail.com>
2024-03-03 20:50:12 +0700

    Perubahan pada praktikum 5 (Query Builder)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Praktikum 4
 Route::get('/level', [LevelController::class, 'index']);
+
+//Praktikum 5
+Route::get('/kategori', [KategoriController::class, 'index']);
