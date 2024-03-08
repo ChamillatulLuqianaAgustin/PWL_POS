bc25772aefa62596a2c8e4c4dfb43e873cb170ca
ChamillatulLuqianaAgustin <chamillaluqiana@gmail.com>
2024-03-08 14:25:31 +0700

    Perubahan pada JS 4 Praktikum 2.3 (Retreiving Aggregrates)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,7 +39,11 @@ class UserController extends Controller
         // $user = UserModel::findOrFail(1);
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::where('level_id', 2)->get();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 }
