d0abe85402e632ea8d00a87acb86b6ac02d14025
ChamillatulLuqianaAgustin <chamillaluqiana@gmail.com>
2024-03-11 01:46:20 +0700

    Perubahan pada JS 4 Praktikum 2.6 (CRUD)

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Praktikum 4
+//Praktikum 4 JS 3
 Route::get('/level', [LevelController::class, 'index']);
 
-//Praktikum 5
+//Praktikum 5 JS 3
 Route::get('/kategori', [KategoriController::class, 'index']);
 
-//Praktikum 6
+//Praktikum 6 JS 3
 Route::get('/user', [UserController::class, 'index']);
+
+//Praktikum 2.6 JS 4
+Route::get('/user/tambah', [UserController::class, 'tambah'])->name('user.tambah');
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah');
+Route::get('/user', [UserController::class, 'index'])->name('user.index'); //Kembali
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('user.hapus');
