eb9cb8efa490589ec763e80fa934cdc1149a087d
ChamillatulLuqianaAgustin <chamillaluqiana@gmail.com>
2024-04-05 15:32:51 +0700

    Jobsheet 7 - Tugas (Menu Transaksi Penjualan)

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\POSController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\StokController;
+use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -56,14 +58,36 @@ Route::group(['prefix' => 'kategori'], function () {
 });
 
 Route::group(['prefix' => 'barang'], function () {
-    Route::get('/', [BarangController::class, 'index']);          // Menampilkan halaman awal kategori
-    Route::post('/list', [BarangController::class, 'list']);      // Menampilkan data kategori dalam bentuk json untuk datatables
-    Route::get('/create', [BarangController::class, 'create']);   // Menampilkan halaman form tambah kategori
-    Route::post('/', [BarangController::class, 'store']);         // Menyimpan data kategori baru
-    Route::get('/{id}', [BarangController::class, 'show']);       // Menampilkan detail kategori
-    Route::get('/{id}/edit', [BarangController::class, 'edit']);  // Menampilkan halaman form edit kategori
-    Route::put('/{id}', [BarangController::class, 'update']);     // Menyimpan perubahan data kategori
-    Route::delete('/{id}', [BarangController::class, 'destroy']); // Menghapus data kategori
+    Route::get('/', [BarangController::class, 'index']);          // Menampilkan halaman awal barang
+    Route::post('/list', [BarangController::class, 'list']);      // Menampilkan data barang dalam bentuk json untuk datatables
+    Route::get('/create', [BarangController::class, 'create']);   // Menampilkan halaman form tambah barang
+    Route::post('/', [BarangController::class, 'store']);         // Menyimpan data barang baru
+    Route::get('/{id}', [BarangController::class, 'show']);       // Menampilkan detail barang
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);  // Menampilkan halaman form edit barang
+    Route::put('/{id}', [BarangController::class, 'update']);     // Menyimpan perubahan data barang
+    Route::delete('/{id}', [BarangController::class, 'destroy']); // Menghapus data barang
+});
+
+Route::group(['prefix' => 'stok'], function () {
+    Route::get('/', [StokController::class, 'index']);          // Menampilkan halaman awal stok
+    Route::post('/list', [StokController::class, 'list']);      // Menampilkan data stok dalam bentuk json untuk datatables
+    Route::get('/create', [StokController::class, 'create']);   // Menampilkan halaman form tambah stok
+    Route::post('/', [StokController::class, 'store']);         // Menyimpan data stok baru
+    Route::get('/{id}', [StokController::class, 'show']);       // Menampilkan detail stok
+    Route::get('/{id}/edit', [StokController::class, 'edit']);  // Menampilkan halaman form edit stok
+    Route::put('/{id}', [StokController::class, 'update']);     // Menyimpan perubahan data stok
+    Route::delete('/{id}', [StokController::class, 'destroy']); // Menghapus data stok
+});
+
+Route::group(['prefix' => 'transaksi'], function () {
+    Route::get('/', [TransaksiController::class, 'index']);          // Menampilkan halaman awal transaksi
+    Route::post('/list', [TransaksiController::class, 'list']);      // Menampilkan data transaksi dalam bentuk json untuk datatables
+    Route::get('/create', [TransaksiController::class, 'create']);   // Menampilkan halaman form tambah transaksi
+    Route::post('/', [TransaksiController::class, 'store']);         // Menyimpan data transaksi baru
+    Route::get('/{id}', [TransaksiController::class, 'show']);       // Menampilkan detail transaksi
+    Route::get('/{id}/edit', [TransaksiController::class, 'edit']);  // Menampilkan halaman form edit transaksi
+    Route::put('/{id}', [TransaksiController::class, 'update']);     // Menyimpan perubahan data transaksi
+    Route::delete('/{id}', [TransaksiController::class, 'destroy']); // Menghapus data transaksi
 });
 
 // Route::get('/', function () {
