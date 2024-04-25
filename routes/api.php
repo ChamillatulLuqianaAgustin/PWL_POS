a2bf46fbf1969b25d3e4392418a45c8f3a96f394
ChamillatulLuqianaAgustin <chamillaluqiana@gmail.com>
2024-04-26 00:24:50 +0700

    Jobsheet 10 - Praktikum 3 (Membuat RESTful API Logout)

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,14 @@ use Illuminate\Support\Facades\Route;
 // Register
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 
-//Login
+// Login
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Logout
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
