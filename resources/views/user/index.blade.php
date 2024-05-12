35f0be63de932e785539642dd58afa1864d75e7f
ChamillatulLuqianaAgustin <chamillaluqiana@gmail.com>
2024-05-13 02:58:53 +0700

    Jobsheet 12 - Tugas 2 (Tabel m_user)

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -38,6 +38,7 @@
                         <th>Username</th>
                         <th>Nama</th>
                         <th>Level Pengguna</th>
+                        <th>Foto Pengguna</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
@@ -83,6 +84,14 @@
                         className: "",
                         orderable: false, // orderable: true, jika ingin kolom ini bisa diurutkan
                         searchable: false // searchable: true, jika ingin kolom ini bisa dicari
+                    }, {
+                        data: "image",
+                        className: "",
+                        orderable: false,
+                        searchable: false,
+                        render: function(data, type, row) {
+                            return '<img src="' + data + '" alt="Image" class="img-thumbnail" width="100">';
+                        }
                     }, {
                         data: "aksi", 
                         className: "",
